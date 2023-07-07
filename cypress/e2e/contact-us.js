f2cc4642e441a2b4a71ecd5e1a75bc4ce1a9308f
StehCastell <53917285+StehCastell@users.noreply.github.com>
2023-07-07 00:05:30 -0300

    environment preparation - 02

---
+++ cypress/e2e/contact-us.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe("Test Contact Us form via WebdriverUni", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
+        //cypress code
+        cy.click
+    });
+
+    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+        //cypress code
+    })
+})
